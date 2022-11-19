a7c6738499ec23d5de19f00d54b0c4a404799630
Querelle <andrea.db@gmx.it>
2022-11-19 15:21:08 +0100

    feat: header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+const COMPONENTS = [HeaderComponent];
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, ...COMPONENTS],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
